67779f7906a3e139f7b64673b72ff5284f74286a
Kimutai <jankimsingoei@gmail.com>
2024-06-03 22:33:33 +0300

    Update navbar to include user logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaUser,FaBars } from 'react-icons/fa';
-import { FaUserTie } from "react-icons/fa6";
+import { FaBars } from 'react-icons/fa';
+import defaultAvatar from '../Assets/Excited boy with kinky hair flat vector avatar icon with green dot_ Editable default per.jpeg'; 
 import '../Style/navbar.css';
 
 function Navbar({ userName, toggleSidebar }) {
   return (
     <nav className="navbar">
-        <div className="navbar-menu">
+      <div className="navbar-menu">
         <FaBars className="hamburger" onClick={toggleSidebar} />
       </div>
       <div className="navbar-brand">
-      <Link to="/">Sing'oei AgroLog</Link>
+        <Link to="/">Sing'oei AgroLog</Link>
       </div>
       <ul className="navbar-nav">
-        
         <li className="nav-item user-info">
-          <FaUserTie /> {userName}
+          <img src={defaultAvatar} alt="User Avatar" className="user-avatar" />
+          {userName}
         </li>
       </ul>
     </nav>
