d68b974a82713518f3e9e771947dd27d008bf411
Kimutai <jankimsingoei@gmail.com>
2024-06-05 15:24:53 +0300

    Moved the hamburger menu from the sidebar and improved the layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import defaultAvatar from '../Assets/Excited boy with kinky hair flat vector avatar icon with green dot_ Editable default per.jpeg'; 
 import '../Style/navbar.css';
 
-function Navbar({ userName, toggleSidebar }) {
+function Navbar({ userName, isVisible, toggleSidebar }) {
   return (
     <nav className="navbar">
       <div className="navbar-menu">
-        <FaBars className="hamburger" onClick={toggleSidebar} />
+        <button className="menu-btn" onClick={toggleSidebar}>
+          {isVisible ? <FaTimes /> : <FaBars />}
+        </button>
       </div>
       <div className="navbar-brand">
-        <Link to="/">Sing'oei AgroLog</Link>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          Sing'oei AgroLog
+        </Link>
       </div>
       <ul className="navbar-nav">
         <li className="nav-item user-info">
           <img src={defaultAvatar} alt="User Avatar" className="user-avatar" />
-          {userName}
+          <span className='user'>{userName}</span>
         </li>
       </ul>
     </nav>
